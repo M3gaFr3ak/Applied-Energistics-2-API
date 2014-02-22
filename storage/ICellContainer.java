ffd08139bd0e97a287e5e2391597e219e77e0179
M3gaFr3ak <leon.loeser@yahoo.de>
2014-02-22 19:46:42 +0100

    Un-Uncheck Types the Third

--- storage/ICellContainer.java
+++ storage/ICellContainer.java
@@ -18,7 +18,7 @@ public interface ICellContainer extends IActionHost
 	 * 
 	 * @return a valid list of handlers, NEVER NULL
 	 */
-	List<IMEInventoryHandler> getCellArray(StorageChannel channel);
+	<T extends IAEStack> List<IMEInventoryHandler<T>> getCellArray();
 
 	/**
 	 * the storage's priority.
